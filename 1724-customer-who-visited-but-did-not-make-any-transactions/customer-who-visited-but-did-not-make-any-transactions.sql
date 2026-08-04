029729f3e253f3feafe8edd619bbe7872288948f
Vikrant <vikrantvaidya17@gmail.com>
2026-08-04 22:12:57 +0530

    Time: 484 ms (64.34%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
-SELECT customer_id,COUNT(*) as count_no_trans FROM Visits WHERE visit_id NOT IN (SELECT visit_id FROM Transactions) GROUP BY customer_id;
+-- Write your PostgreSQL query statement below
+SELECT v.customer_id,COUNT(v.visit_id) as count_no_trans FROM Visits v LEFT JOIN Transactions t ON v.visit_id=t.visit_id WHERE t.transaction_id IS NULL GROUP BY customer_id; 
